e9d988819f24bcb2409500121d671238b5d79f66
romaric-demide <romaricdemide7@gmail.com>
2025-05-14 22:00:22 +0100

    feat: define PostCardProps type for better type safety in PostCard component

--- components/post-card.tsx
+++ components/post-card.tsx
@@ -1,4 +1,30 @@
-export default function PostCard({ post }: { post: any }) {
+type PostCardProps = {
+  id: string;
+  text: string;
+  images: string[];
+  userId: string;
+  user: {
+    id: string;
+    username: string;
+    image: string;
+    followers: { followerId: string }[];
+    following: { followingId: string }[];
+    _count: {
+      followers: number;
+      posts: number;
+    };
+  };
+  likes: { userId: string }[];
+  saves: { userId: string }[];
+  _count: {
+    likes: number;
+    replies: number;
+  };
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export default function PostCard({ post }: { post: PostCardProps }) {
   return (
     <div>
       <div>Post Card</div>
