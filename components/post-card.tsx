b6a20581a18437d56f70ec379abca4bec71e82e8
romaric-demide <romaricdemide7@gmail.com>
2025-05-15 09:09:04 +0100

    feat: add Drawer and Dropdown Menu components
    - Added Drawer component using vaul library for better UI management.
    - Introduced Dropdown Menu component utilizing @radix-ui/react-dropdown-menu for enhanced dropdown functionality.
    - Updated package.json to include new dependencies: @radix-ui/react-dialog, @radix-ui/react-dropdown-menu, and vaul.

--- components/post-card.tsx
+++ components/post-card.tsx
@@ -1,3 +1,15 @@
+"use client";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerTitle
+} from "@/components/ui/drawer";
+import { DotIcon, EllipsisIcon } from "lucide-react";
+import Image from "next/image";
+import { useState } from "react";
+
 type PostCardProps = {
   id: string;
   text: string;
@@ -25,9 +37,66 @@ type PostCardProps = {
 };
 
 export default function PostCard({ post }: { post: PostCardProps }) {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div>
-      <div>Post Card {post.text}</div>
+    <div className="flex space-x-2 px-4">
+      <Avatar onClick={() => setOpen(true)}>
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+
+      <div className="flex-1">
+        <div className="flex items-center justify-between">
+          <div className="flex">
+            <button className="font-bold" onClick={() => setOpen(true)}>
+              @{post.user.username}
+            </button>
+
+            <div className="text-muted-foreground flex items-center">
+              <DotIcon className="size-5" /> 4h
+            </div>
+          </div>
+
+          <button className="bg-amber-300">
+            <EllipsisIcon className="size-5" />
+          </button>
+        </div>
+
+        <div>
+          <div>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est natus
+            eum culpa sunt distinctio vitae nihil, nisi sit illo molestiae
+            maiores officiis, neque a dicta in rem repellat exercitationem
+            explicabo.
+          </div>
+
+          <div className="flex aspect-square flex-wrap overflow-hidden">
+            {Array.from({ length: 3 }).map((_, i) => (
+              <div key={i} className="relative aspect-square flex-1 basis-1/2">
+                <Image
+                  src="https://sje25hcba2nnaaxl.public.blob.vercel-storage.com/MainBefore-SAA8uV3odbMA23hsfDPNTPXrF3F0Qv.jpg"
+                  alt="Next.js logo"
+                  fill
+                  priority
+                  className="object-cover"
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div>footer</div>
+      </div>
+
+      {/* drawer */}
+      <Drawer open={open} onOpenChange={setOpen}>
+        <DrawerContent>
+          <DrawerTitle className="sr-only" />
+
+          <div>Contenu</div>
+        </DrawerContent>
+      </Drawer>
     </div>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      new URL("https://sje25hcba2nnaaxl.public.blob.vercel-storage.com/**"),
+    ],
+  },
 };
 
 export default nextConfig;
