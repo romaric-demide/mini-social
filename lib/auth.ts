7ef24b4ee8ba7a0447e38e807916543beef094a6
romaric-demide <romaricdemide7@gmail.com>
2025-06-06 02:13:28 +0100

    feat: ajouter la configuration de Prisma et le fournisseur Google pour l'authentification

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,21 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import prisma from "./prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Google({
+      profile(profile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+          username: profile.name?.toLowerCase().replace(/\s+/g, ""),
+        };
+      },
+    }),
+  ],
 });
